4f99ffb2789e4c6e35b7505712a06c2d8cbf3230
童永泽 <604232797@qq.com>
2021-01-05 20:36:32 +0800

    完成了首页功能开发

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+$http.beforeRequest = function (options) {
+  wx.showLoading({
+    title:'数据加载中...'
+  })
+}
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title = "数据请求失败！",duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
