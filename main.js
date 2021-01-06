e7ff7b3cf050ab1641318da44770f768cded4981
童永泽 <604232797@qq.com>
2021-01-06 16:34:08 +0800

    完成了分类页面的开发

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ $http.afterRequest = function() {
 }
 
 // 封装弹框的方法
-uni.$showMsg = function(title = "数据请求失败！",duration = 1500) {
+uni.$showMsg = function(title = "数据请求失败！", duration = 1500) {
   uni.showToast({
     title,
     duration,
